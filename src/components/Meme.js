5bcc29dcbb963494d24a220b2e82ee1875a823e2
hsmgowtham <hsmgowtham@gmail.com>
2024-05-26 05:14:39 -0400

    Add GetRandomURL from meme.js to console onClick

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import meme_data from '../memesData.js'
 
 export default function Meme() {
+    function getRandomMemeURL(event) {
+        event.preventDefault();
+        const memes = meme_data.data.memes;
+        const randomIndex = Math.floor(Math.random() * memes.length);
+        console.log(randomIndex)
+        const url = memes[randomIndex].url;
+        console.log(url)
+    }
     return (
         <main>
             <section className='section--form'>
                 <form className='form'>
-                    
+
                     <input type='text' className='form--inputs' placeholder='Enter top text' />
                     <input type='text' className='form--inputs' placeholder='Enter Bottom text' />
 
-                    <button className='form--button' type='submit'>Get a new meme image 🖼</button>
+                    <button onClick={getRandomMemeURL} className='form--button'>Get a new meme image 🖼</button>
                 </form>
             </section>
 
